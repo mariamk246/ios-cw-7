f5383418502e057019d6b0fe9f6449c071577d8f
mariam <mariooom456@gmail.com>
2020-10-06 08:31:22 +0300

    اسفة على التاخير

---
+++ Classwork6/MovieMasterCV.swift
@@ -0,0 +1,31 @@
+//
+//  MovieMasterCV.swift
+//  Classwork6
+//
+//  Created by Mariam Alkandari on 10/4/20.
+//  Copyright © 2020 Hasan Alsaffar. All rights reserved.
+//
+
+import UIKit
+
+class MovieMasterCV: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("app opened!")
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
